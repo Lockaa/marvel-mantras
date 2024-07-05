6c4a2ca4b37e6b34ccb4431551d5fe9bad450054
Iggys4ur <eswaind@gmail.com>
2024-07-05 14:14:39 -0400

    implemented bulma on index.html; revised script.js

--- js/script.js
+++ js/script.js
@@ -1,27 +1,18 @@
-const searchBtn = document.querySelector('#search-btn');
-const charInput = document.querySelector('#char-input');
+const MARVEL_API_KEY = 'ce26b3bbf1d31409a66690dc30f25651cd37ad9e';
+const MARVEL_API_HASH = '9f1125689ab84def1b3598021fd55e39';
+const QUOTE_API_URL = 'https://api.quotable.io/random';
 
-function outputSearch() {
-  const charInput = document.querySelector('#char-input');
-  const apiKey = 'b32943396b0e19ebdc4710f3fe2bfc10';
-  const quotesapiKey = '4777e1cf53msh57faac29ab5b6c9p179fa4jsn8940b5e2cc2c'
-  const charactersUrl = `https://gateway.marvel.com/v1/public/characters?apikey=${apiKey}`;
-  const quotesUrl = `https://marvel-quote-api.p.rapidapi.com/`
+const characterContainer = document.getElementById('character-container');
+const quoteContainer = document.getElementById('quote-container');
+const generateButton = document.getElementById('generate-button');
 
-
-  $.get(charactersUrl)
-    .then(function (charactersData) {
-      console.log('Characters Date:', charactersData);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-  }
-// fetch(quotesUrl)
-//   .then(function (quotes))
-
-
-
-
-outputSearch();
-
+// Fetch random Marvel character
+async function fetchMarvelCharacter() {
+    let test = (new Date()).getMilliseconds();
+    let timestamp = Number(new Date());
+    console.log(test, timestamp);
+    const response = await fetch(`http://gateway.marvel.com/v1/public/comics?ts=${timestamp}&apikey=${MARVEL_API_KEY}&hash=${MARVEL_API_HASH}`);
+    const data = await response.json();
+    const character = data.data.results[Math.floor(Math.random() * data.data.results.length)];
+    return character;
+}
