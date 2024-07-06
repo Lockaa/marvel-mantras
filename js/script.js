f15a19717d85d91ff5d59b2f4fe995c3b2c19542
Iggys4ur <eswaind@gmail.com>
2024-07-06 05:02:05 -0400

    it works! (thanks to CryptoJS)

--- js/script.js
+++ js/script.js
@@ -1,27 +1,25 @@
-const MARVEL_API_KEY = 'fac6d7c1ee74188e67b2d1496798be03';
-const MARVEL_PRIVATE_API_KEY = 'ce26b3bbf1d31409a66690dc30f25651cd37ad9e';
-const MARVEL_API_HASH = '9f1125689ab84def1b3598021fd55e39';
-const QUOTE_API_URL = 'https://api.quotable.io/random';
+const publicKey = 'fac6d7c1ee74188e67b2d1496798be03';
+const privateKey = 'ce26b3bbf1d31409a66690dc30f25651cd37ad9e';
+const timestamp = new Date().getTime();
+const hash = CryptoJS.MD5(timestamp + privateKey + publicKey).toString(); // Using CryptoJS for MD5 hashing
 
-const characterContainer = document.getElementById('character-container');
-const quoteContainer = document.getElementById('quote-container');
-const generateButton = document.getElementById('generate-button');
+// Base URL for Marvel API
+const baseURL = 'https://gateway.marvel.com:443/v1/public/characters';
 
-// Fetch random Marvel character
-async function fetchMarvelCharacter() {
-    let test = new Date().getTime();
-    console.log(test);
-    // fetch with timestamp, key, and md5 key hash (timestamp|public key|private key)
-    // const response = await fetch(`http://gateway.marvel.com/v1/public/comics?ts=${timestamp}&apikey=${MARVEL_API_KEY}&hash=${MARVEL_API_HASH}`);
-    // fetch with key
-    requestURL = `http://gateway.marvel.com/v1/public/comics?apikey=${MARVEL_API_KEY}`;
-    const response = await fetch(requestURL);
+// Function to get characters
+async function getMarvelCharacters() {
+    
+    const url = `${baseURL}?ts=${timestamp}&apikey=${publicKey}&hash=${hash}`;
+    const response = await fetch(url);
     const data = await response.json();
-    // generate a random index within the bounds of the array length
-    const charIndex = Math.floor(Math.random() * data.data.results.length);
-    // retrieve the character stored at that random index
-    const character = data.data.results[charIndex];
-    return character;
+    
+    // Store data in local storage
+    localStorage.setItem('marvelCharacters', JSON.stringify(data.data.results));
+    return data.data.results; // Array of characters
 }
 
-generateButton.addEventListener('click', fetchMarvelCharacter);
+// Usage
+getMarvelCharacters().then(characters => {
+    console.log('Marvel Characters:', characters);
+    // You can now work with the list of characters
+});
