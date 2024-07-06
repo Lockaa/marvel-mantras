dc9553ce5ca37267d8ddbd4bf9ec996b2872ec1a
Lockaa <143623393+Lockaa@users.noreply.github.com>
2024-07-06 15:04:18 -0400

    Allen-new-quotes-api

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
-
 const searchBtn = document.querySelector('#search-btn');
 const charInput = document.querySelector('#char-input');
 
 function outputSearch() {
   const charInput = document.querySelector('#char-input');
   const apiKey = 'b32943396b0e19ebdc4710f3fe2bfc10';
-  //const quotesapiKey = '4777e1cf53msh57faac29ab5b6c9p179fa4jsn8940b5e2cc2c'
+  const quotesapiKey = '4777e1cf53msh57faac29ab5b6c9p179fa4jsn8940b5e2cc2c'
   const charactersUrl = `https://gateway.marvel.com/v1/public/characters?apikey=${apiKey}`;
-  //const quotesUrl = `https://marvel-quote-api.p.rapidapi.com/`
+  const quotesUrl = `https://marvel-quote-api.p.rapidapi.com/`
 
 
   $.get(charactersUrl)
