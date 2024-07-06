101fb25cb199f52043e2dccab5aa0fee667510d9
Lockaa <143623393+Lockaa@users.noreply.github.com>
2024-07-06 13:42:21 -0400

    allen-quotes-work

--- js/script.js
+++ js/script.js
@@ -1,19 +1,34 @@
+
 const searchBtn = document.querySelector('#search-btn');
 const charInput = document.querySelector('#char-input');
 
 function outputSearch() {
   const charInput = document.querySelector('#char-input');
   const apiKey = 'b32943396b0e19ebdc4710f3fe2bfc10';
-
-  const url = `https://gateway.marvel.com/v1/public/characters?apikey=${apiKey}`;
-
-  $.get(url)
-    .then(function(data) {
-      console.log(data);
+  //const quotesapiKey = '4777e1cf53msh57faac29ab5b6c9p179fa4jsn8940b5e2cc2c'
+  const charactersUrl = `https://gateway.marvel.com/v1/public/characters?apikey=${apiKey}`;
+  //const quotesUrl = `https://marvel-quote-api.p.rapidapi.com/`
 
 
+  $.get(charactersUrl)
+    .then(function (charactersData) {
+      console.log('Characters Date:', charactersData);
     })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+// fetch(quotesUrl)
+//   .then(function (quotes))
+
+function getQuote() {
+  fetch("https://type.fit/api/quotes")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
+  });
 }
 
-outputSearch()
-
+outputSearch();
