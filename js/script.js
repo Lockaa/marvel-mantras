07a6b8f9a77ad59b9e353236a528fae6d89905db
arizvi-prog <alishajrizvi@gmail.com>
2024-07-08 11:30:05 -0400

    alisha-animation-workspace

--- js/script.js
+++ js/script.js
@@ -129,6 +129,40 @@ function getSavedCharacter() {
   }
 }
 
+// Animation function
+const color = document.getElementById('colored');
+let x = 0;
+
+window.addEventListener('load', function() {
+  setTimeout(function() {
+    doFirst(0);
+  }, 500);
+}, false);
+
+function doFirst(x) {
+  if (x === 0) {
+    color.style.clipPath = "polygon(0 0,100% 0,100% 100%,0 100%)";
+    color.style.transition = "clip-path .8s";
+    setTimeout(function() {
+      color.style.clipPath = "polygon(99.99% 0,100% 0,100% 100%,99.99% 100%)";
+      color.style.transition = "clip-path .8s";
+    }, 900);
+    x = 99.99;
+  } else {
+    color.style.clipPath = "polygon(0 0,100% 0,100% 100%,0 100%)";
+    color.style.transition = "clip-path .8s";
+    setTimeout(function() {
+      color.style.clipPath = "polygon(0 0,0.01% 0,0.01% 100%,0 100%)";
+      color.style.transition = "clip-path .8s";
+    }, 900);
+    x = 0;
+  }
+
+  setTimeout(function() {
+    doFirst(x);
+  }, 1900);
+}
+
 // Event listeners
 const generateBtn = document.getElementById('generate-character');
 generateBtn.addEventListener('click', fetchRandomWithPicture);
@@ -166,4 +200,5 @@ multipleBtn.addEventListener('click', () => {
     console.log('Please enter both limit and offset.');
   }
 });
+
 
