e6756d34f9870f835f7e18cbf5bf1b8606cce928
arizvi-prog <alishajrizvi@gmail.com>
2024-07-08 20:52:56 -0400

    current-code-workspace

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,7 @@ async function fetchCharacterByIndex(index) {
   // while (response.) {
   // }
   const data = await response.json();
-  
+
 
   //while awaiting response: call animation
 
@@ -107,33 +107,33 @@ function getSavedCharacter() {
   }
 }
 
+
 // Animation JS
+var color = document.getElementById('colored');
+var x;
+window.addEventListener('load', function () { setTimeout(function () { doFirst(0); }, 500) }, false);
+function doFirst(x) {
+  if (x == 0) {
+    color.style.clipPath = "polygon(0 0,100% 0,100% 100%,0 100%)";
+    color.style.transition = "clip-path .8s";
+    setTimeout(function () {
+      color.style.clipPath = "polygon(99.99% 0,100% 0,100% 100%,99.99% 100%)";
+      color.style.transition = "clip-path .8s";
+    }, 900);
+    x = 99.99;
+  }
+  else {
+    color.style.clipPath = "polygon(0 0,100% 0,100% 100%,0 100%)";
+    color.style.transition = "clip-path .8s";
+    setTimeout(function () {
+      color.style.clipPath = "polygon(0 0,0.01% 0,0.01% 100%,0 100%)";
+      color.style.transition = "clip-path .8s";
+    }, 900);
+    x = 0;
+  }
 
-var color=document.getElementById('colored');
-      var x;
-      window.addEventListener('load',function(){setTimeout(function(){doFirst(0);},500)},false);
-      function doFirst(x){
-        if(x==0)
-        {
-          color.style.clipPath="polygon(0 0,100% 0,100% 100%,0 100%)";
-          color.style.transition="clip-path .8s";
-          setTimeout(function(){
-            color.style.clipPath="polygon(99.99% 0,100% 0,100% 100%,99.99% 100%)";
-            color.style.transition="clip-path .8s";
-          },900);
-          x=99.99;
-        }
-        else {
-          color.style.clipPath="polygon(0 0,100% 0,100% 100%,0 100%)";
-          color.style.transition="clip-path .8s";
-          setTimeout(function(){
-            color.style.clipPath="polygon(0 0,0.01% 0,0.01% 100%,0 100%)";
-            color.style.transition="clip-path .8s";
-          },900);
-          x=0;
-        }
-
-        setTimeout(function(){doFirst(x);},1900);}
+  setTimeout(function () { doFirst(x); }, 1900);
+}
 
 // Event listeners
 document.addEventListener('DOMContentLoaded', () => {
